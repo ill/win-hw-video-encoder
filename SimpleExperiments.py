e3fdd8e03fa7060e6e8440b639245cea83c609c7
Ilya Seletsky <iseletsky@roblox.com>
2025-10-10 18:06:16 -0700

    Some adjustments after debugging.

--- SimpleExperiments.py
+++ SimpleExperiments.py
@@ -27,8 +27,8 @@ def main():
     #CRFExperiment.CRFExperiment('zootopia12.mp4', 'zootopia12').run_experiment()
 
     CRFExperiment.CRFExperiment('bipbop15_270_mono.mp4', 'bipbop15_270_mono').run_experiment()
+    CRFExperiment.CRFExperiment('1440p-av1-42sec.mp4', '1440p-av1-42sec').run_experiment()
     CRFExperiment.CRFExperiment('sonichd.mp4', 'sonichd').run_experiment()
-    #CRFExperiment.CRFExperiment('1440p-av1-42sec.mp4', '1440p-av1-42sec').run_experiment()
 
     #CQGoogleExperiment.CQGoogleExperiment('bipbop15_270_mono.mp4', 'bipbop15_270_mono').run_experiment()
     #CQGoogleExperiment.CQGoogleExperiment('1440p-av1-42sec.mp4', '1440p-av1-42sec').run_experiment()
